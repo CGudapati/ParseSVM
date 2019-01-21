d5f241e32148d375f249adfc6ac377fd42529740
acco <luca.accorsi93@gmail.com>
2019-01-21 11:47:38 +0100

    Removed vector init at every split call

--- ParseSVM/ParseSVM.cpp
+++ ParseSVM/ParseSVM.cpp
@@ -6,19 +6,20 @@
 //  Copyright © 2019 Naga V Gudapati. All rights reserved.
 //
 
+#include <iterator>
 #include "ParseSVM.hpp"
 
 //#code taken from fluent cpp which splits a string into a vector using delimiters
-std::vector<std::string> split(const std::string& s, char delimiter)
+void split(std::vector<std::string>& tokens, const std::string& s, char delimiter)
 {
-    std::vector<std::string> tokens;
+    tokens.clear();
     std::string token;
     std::istringstream tokenStream(s);
     while (std::getline(tokenStream, token, delimiter))
     {
         tokens.push_back(token);
     }
-    return tokens;
+
 }
 
 
@@ -27,7 +28,9 @@ std::vector<std::string> split(const std::string& s, char delimiter)
 void get_CRSM_from_svm(CRS_Matrix &M, const std::string &file_path){
     std::cout << "Starting the processing of the SVM file" << std::endl;
     std::string path =  file_path;
-    
+
+    std::vector<std::string> tokens(2);
+
     std::ifstream libsvm_file(path);
     if (libsvm_file.is_open()) {
         std::cout << "Processing the SVM file" << std::endl;
@@ -36,16 +39,17 @@ void get_CRSM_from_svm(CRS_Matrix &M, const std::string &file_path){
         M.n = 0; //This will be used to store the number of columns
         while (getline(libsvm_file, observation)) {
 
-            //Splitting on whitespace as some SVMS have more than one space charcater or a tab character
+            //Splitting on whitespace as some SVMS have more than one space character or a tab character
             std::istringstream iss_obs(observation);
             std::vector<std::string> splitString(std::istream_iterator<std::string>{iss_obs}, std::istream_iterator<std::string>());
-            //I am pusing back the label to the y_label vector
+            //I am pushing back the label to the y_label vector
             M.y_label.push_back(std::stoi(splitString[0]));
             
-//            This will iterate from the second eleemnet onwards, then split at : and push the first
+//            This will iterate from the second element onwards, then split at : and push the first
 //            value into col_index and second values into the values vectors.
             for (auto iter = std::next(splitString.begin()); iter != splitString.end(); ++iter) {
-                auto col_value = split(*iter, ':');
+                split(tokens, *iter, ':');
+                auto& col_value = tokens;
                 M.col_index.push_back(std::stoi(col_value[0])-1);
                 M.values.push_back(std::stod(col_value[1]));
                 if (M.n < std::stoi(col_value[0])) {  //We keep track of the largest n which will give us the value of largest feature number
